ea9ccabe136bc80b61ce442721c9229104ca2307
cassiniXu <cassini.Xu@outlook.com>
2021-03-24 18:40:13 -0400

    add js/config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_FMzWvvWK5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'vp3n0vsrqv4oam6vv4lgr9bfq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
