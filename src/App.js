62b27851cde326c6192b226f706b94ef24a6ee82
cassiniXu <cassini.Xu@outlook.com>
2021-03-28 16:14:19 -0400

    login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+function SignedInComponent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          daffsdfsdfdfs <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className = "section">
+      <div className = "container has-text-centered">
+        Hello username!
+        You're now signed in
+      </div>
+    </section>
+  )
 }
+
+function App(){
+  return (
+    <section className="section">
+      <div className="container has-text-centered">
+        <h1 className="title"> Twitter for wentaoxu</h1>
+        <div className="subtitle"> A place for wentaoxu to login </div>
+      <button> sign in </button>
+      </div>
+    </section>
+  )
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           daffsdfsdfdfs <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
